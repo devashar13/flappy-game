d1b4fa583014ed97c74a29bff24ef0b0ff12d39b
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2020-11-20 16:16:17 +0530

    Removed main

--- main.py
+++ main.py
@@ -173,35 +173,35 @@ def getRandomPipe():
     ]
     return pipe
 
-if __name__ == "__main__":
+
     
-    pygame.init() 
-    FPSCLOCK = pygame.time.Clock()
-    pygame.display.set_caption('Flappy Bird CSI')
-    GAME_SPRITES['numbers'] = ( 
-        pygame.image.load('gallery/sprites/0.png').convert_alpha(),
-        pygame.image.load('gallery/sprites/1.png').convert_alpha(),
-        pygame.image.load('gallery/sprites/2.png').convert_alpha(),
-        pygame.image.load('gallery/sprites/3.png').convert_alpha(),
-        pygame.image.load('gallery/sprites/4.png').convert_alpha(),
-        pygame.image.load('gallery/sprites/5.png').convert_alpha(),
-        pygame.image.load('gallery/sprites/6.png').convert_alpha(),
-        pygame.image.load('gallery/sprites/7.png').convert_alpha(),
-        pygame.image.load('gallery/sprites/8.png').convert_alpha(),
-        pygame.image.load('gallery/sprites/9.png').convert_alpha(),
-    )
+pygame.init() 
+FPSCLOCK = pygame.time.Clock()
+pygame.display.set_caption('Flappy Bird CSI')
+GAME_SPRITES['numbers'] = ( 
+    pygame.image.load('gallery/sprites/0.png').convert_alpha(),
+    pygame.image.load('gallery/sprites/1.png').convert_alpha(),
+    pygame.image.load('gallery/sprites/2.png').convert_alpha(),
+    pygame.image.load('gallery/sprites/3.png').convert_alpha(),
+    pygame.image.load('gallery/sprites/4.png').convert_alpha(),
+    pygame.image.load('gallery/sprites/5.png').convert_alpha(),
+    pygame.image.load('gallery/sprites/6.png').convert_alpha(),
+    pygame.image.load('gallery/sprites/7.png').convert_alpha(),
+    pygame.image.load('gallery/sprites/8.png').convert_alpha(),
+    pygame.image.load('gallery/sprites/9.png').convert_alpha(),
+)
 
-    GAME_SPRITES['message'] =pygame.image.load('gallery/sprites/message.png').convert_alpha()
-    GAME_SPRITES['base'] =pygame.image.load('gallery/sprites/base.png').convert_alpha()
-    GAME_SPRITES['pipe'] =(
-    pygame.transform.rotate(pygame.image.load( PIPE).convert_alpha(), 180), 
-    pygame.image.load(PIPE).convert_alpha()
-    )
+GAME_SPRITES['message'] =pygame.image.load('gallery/sprites/message.png').convert_alpha()
+GAME_SPRITES['base'] =pygame.image.load('gallery/sprites/base.png').convert_alpha()
+GAME_SPRITES['pipe'] =(
+pygame.transform.rotate(pygame.image.load( PIPE).convert_alpha(), 180), 
+pygame.image.load(PIPE).convert_alpha()
+)
 
-  
-    GAME_SPRITES['background'] = pygame.image.load(BACKGROUND).convert()
-    GAME_SPRITES['player'] = pygame.image.load(PLAYER).convert_alpha()
 
-    while True:
-        welcomeScreen() 
-        mainGame() 
+GAME_SPRITES['background'] = pygame.image.load(BACKGROUND).convert()
+GAME_SPRITES['player'] = pygame.image.load(PLAYER).convert_alpha()
+
+while True:
+    welcomeScreen() 
+    mainGame() 
